8d4d0f893369af7d6aac2d37192e41e42d2ecbb8
Melissa Martinez <melmar12@live.com>
2017-06-28 20:46:07 -0500

    lesson 55: aboutme styles and readme

---
+++ Readme.md
@@ -0,0 +1,2 @@
+## project in Udemy Reactjs course
+view live app [here](https://powerful-basin-63042.herokuapp.com/)
--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,16 @@ var React = require('react');
 // stateless presentational component refactored code
 var About = (props) => {
   return (
-    <h3>About Compnent</h3>
+    <div>
+      <h1 className="text-center">About</h1>
+      <p>This weather app is built with ReactJS</p>
+      <p>here are some of the tools I used:</p>
+      <ul>
+        <li><a href="https://openweathermap.org/">Open Weather Map</a></li>
+        <li><a href="https://heroku.com">Heroku</a></li>
+      </ul>
+      <p>view on <a href="https://github.com/melmar12/Udemy-React-WeatherApp">github</a></p>
+    </div>
   )
 };
 
